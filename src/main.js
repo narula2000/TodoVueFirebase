7d42702a17afc7fdc18a8229c93532263bcaaf0c
Vikrom Narula <vikrom.nar@gmail.com>
2021-02-15 09:59:59 +0700

    imported firbase

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import vuetify from './plugins/vuetify';
+import firebaseConfig from './plugins/firebase';
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
